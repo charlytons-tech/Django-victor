899aa0ce03b6bb745c0e2e979bf458582a42e3d2
Victor <vladworld02@gmail.com>
2026-02-26 12:53:17 +0800

    Django history delete

--- .history/test_project/test_app/views_20260226113710.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-
-def index(request):
-    return HttpResponse("Hello, Geeks! Welcome to your first Django app.")
-
-
-def healthCheck(request):
-    return HttpResponse()
