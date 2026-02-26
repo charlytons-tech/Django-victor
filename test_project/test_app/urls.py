659e462972f097b570b3b4cafb3c3a4121e556ad
Victor <vladworld02@gmail.com>
2026-02-26 14:05:42 +0800

    Django setup

--- test_project/test_app/urls.py
+++ test_project/test_app/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('test_project/healthcheck', views.healthcheck, name='healthCheck')
+    path('healthcheck/', views.healthcheck, name='healthCheck')
 ]
